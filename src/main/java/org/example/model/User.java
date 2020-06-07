6e53cc48c36ea86b544c56496c4cb1ba35f61077
jay <jay@ugps-MBP.attlocal.net>
2020-06-07 18:35:34 -0400

    add anotation to user class, add hibernate, remove extra code in UserDao

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,28 +1,62 @@
 package org.example.model;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
+@Entity
+@Table(name = "users")
 public class User {
-    public User(){}
+    public  User(){}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private long userId;
+    @Column(name = "email")
     private String email;
+    @Column(name = "password")
     private String password;
+    @Column(name = "user_name")
     private String username;
+    @Column(name = "created_on")
     private Timestamp createdOn;
+
+    public long getUserId() {
+        return userId+super.hashCode();
+    }
+
     public void setUserId (long userId){
         this.userId = userId;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail (String email){
         this.email = email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password){
         this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public void setUserName(String username){
         this.username = username;
     }
+
+    public Timestamp getCreatedOn() {
+        return createdOn;
+    }
     public void setCreatedOn(Timestamp createdOn){
         this.createdOn = createdOn;
     }
 }
+
--- src/main/java/org/example/repository/UserDao.java
+++ src/main/java/org/example/repository/UserDao.java
@@ -7,7 +7,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class UserDao {
