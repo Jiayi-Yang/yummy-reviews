af20717bca34e33b8d23eb3c0ca9d78cba3e0e9d
jay <jay@ugps-MacBook-Pro.local>
2020-07-01 19:39:30 -0400

    change dao to interface, add dao impl and unit test

---
+++ src/main/java/org/example/repository/UserDaoImpl.java
@@ -0,0 +1,97 @@
+package org.example.repository;
+
+import org.example.model.User;
+import org.example.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoImpl implements UserDao{
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @Override
+    public User save(User user) {
+        Transaction transaction = null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session s = sessionFactory.openSession();
+        try {
+            transaction = s.beginTransaction();
+            s.save(user);
+            transaction.commit();
+            s.close();
+            return user;
+        } catch (Exception e){
+            if (transaction != null) transaction.rollback();
+            logger.error("fail to insert record");
+            s.close();
+            return null;
+        }
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(User user) {
+        String hql = "DELETE User as u WHERE u.email = :Email";
+        int deletedCount = 0;
+        Transaction transaction = null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session s = sessionFactory.openSession();
+        try {
+            transaction = s.beginTransaction();
+            Query<User> query = s.createQuery(hql);
+            query.setParameter("Email", user.getEmail());
+            deletedCount = query.executeUpdate();
+            transaction.commit();
+            s.close();
+            return deletedCount >= 1 ? true : false;
+        } catch (HibernateException e){
+            if (transaction != null) transaction.rollback();
+            s.close();
+            logger.error("unable to delete record", e);
+        }
+        return false;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session s = sessionFactory.openSession();
+        String hql = "FROM User";
+        s.createQuery(hql);
+        List<User> result = new ArrayList<>();
+        try {
+            Query query = s.createQuery(hql);
+            result = query.list();
+            s.close();
+        } catch (HibernateException e){
+            logger.error("session close exception try again", e);
+            s.close();
+        }
+        return result;
+    }
+
+    @Override
+    public User getBy(Long id) {
+        return null;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return null;
+    }
+}
--- src/test/java/org/example/repository/UserDaoTest.java
+++ src/test/java/org/example/repository/UserDaoTest.java
@@ -4,30 +4,37 @@ import org.example.model.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class UserDaoTest {
-
-    UserDao userJDBCDao = new UserDao();
-    //private UserDao userJDBCDao;
+    private UserDao userDao;
+    private User u1;
 
     @Before
-    public void setUp(){
-//        User user = new User();
-//        user.setUserName("testUser");
-//        user.setPassword("123xxx");
-//        user.save(user);
-       // userJDBCDao = new UserDao();
-
+    public void SetUp(){
+        userDao = new UserDaoImpl();
+        u1 = new User();
+        u1.setUserId(0);
+        u1.setEmail("test@gmail.com");
+        u1.setPassword("password");
+        u1.setUsername("test");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        u1.setCreatedOn(timestamp);
+        u1=userDao.save(u1);
     }
     @After
     public void tearDown(){
-//        user.delete();
-//        userJDBCDao=null;
+        userDao.delete(u1);
     }
 
     @Test
     public void getUsersTest(){
-        assertEquals(3,userJDBCDao.getUsers().size());
+        List<User> users = userDao.getUsers();
+        int expectedNumberOfUser = 1;
+        assertEquals(expectedNumberOfUser, users.size());
     }
 }
